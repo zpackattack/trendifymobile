cc4dd04431adecf31f8fc8ac88a5ad0ad2aec401
zpackattack <Treyz3@icloud.com>
2022-10-24 19:21:09 -0400

    Cleaning

--- App.js
+++ App.js
@@ -5,46 +5,12 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { MaterialCommunityIcons } from '@expo/vector-icons';
+import Home from './screens/Home';
+import TopTracks from './screens/TopTracks';
+import TopArtists from './screens/TopArtists';
+import RecentlyPlayed from './screens/RecentlyPlayed';
+
 import navBar from './components/BottomNav.component';
-
-
-function HomeScreen({ navigation }) {
-  return (
-    <View style={styles.homeScreeBackground}>
-      <Text style={styles.homeTitleText}>Home Screen</Text>
-      <Button
-        title="Go to Details"
-        onPress={() => navigation.navigate('Details')}
-      />
-    </View>
-  );
-}
-
-function TopTracks({ navigation }) {
-  return (
-    <View style={styles.homeScreeBackground}>
-      <Text style={styles.homeTitleText}>Details Screen</Text>
-      <Button
-        style = {styles.Buttons}
-        title="Go to Details... again"
-        onPress={() => navigation.navigate('Details')}
-      />
-    </View>
-  );
-}
-
-function TopArtists({ navigation }) {
-  return (
-    <View style={styles.homeScreeBackground}>
-      <Text style={styles.homeTitleText}>Details Screen</Text>
-      <Button
-        style = {styles.Buttons}
-        title="Go to Details... again"
-        onPress={() => navigation.navigate('Details')}
-      />
-    </View>
-  );
-}
 
 const Stack = createNativeStackNavigator();
 const Tab = createBottomTabNavigator();
@@ -61,7 +27,7 @@ function MyTabs() {
     >
       <Tab.Screen
         name="Home"
-        component={HomeScreen}
+        component={Home}
         options={{
           header: () => null,
           tabBarLabel: 'Home',
@@ -94,7 +60,7 @@ function MyTabs() {
       />
       <Tab.Screen
         name="RecentlyPlayed"
-        component={TopArtists}
+        component={RecentlyPlayed}
         options={{
           header: () => null,
           tabBarLabel: 'Recently Played',
@@ -145,60 +111,3 @@ const styles = StyleSheet.create({
   }
   
 });
-
-/*
-export default class App extends React.Component {
-
-  constructor(props){
-    super(props);
-    this.state = { isLoading : true};
-  }
-
-
-  componentDidMount(){
-    return fetch("https://cryptic-basin-76471.herokuapp.com/dreamforce")
-      .then((response) => response.json())
-      .then((responseJson) => {
-        this.setState({
-          isLoading : false,
-          message : responseJson.message
-        });
-      })
-      .catch((error) => {
-        console.error(error);
-      })
-  }
-
-  render() {
-    if(this.state.isLoading){
-      return(
-        <View style = {{flex: 1, padding: 20}}>
-          <ActivityIndicator></ActivityIndicator>
-        </View>
-      )
-    }
-
-    return (
-      <View style = {styles.container}>
-        <Text style = {styles.mainText}>{this.state.message}</Text>
-      </View>
-    );
-  }
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#240530',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  mainText: {
-    paddingTop: 50,
-    flex: 1,
-    color: '#ffffff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
-*/
--- screens/Home.js
+++ screens/Home.js
@@ -1,21 +1,51 @@
-import React from 'react'
+import 'react-native-gesture-handler';
+import * as React from 'react';
+import { Button, View, Text, Pressable, StyleSheet } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
 
-import Body from '../components/home.body'
-import Header from '../components/home.header'
 
 
-
-
-const Home = () =>
-{
-    return(
-        <view>
-            
-            <Text>Hi</Text>
-
-        </view>
+function Home({ navigation }) {
+    return (
+      <View style={styles.homeScreeBackground}>
+        <Text style={styles.homeTitleText}>Home Screen</Text>
+        <Button
+          title="Go to Details"
+          onPress={() => navigation.navigate('Details')}
+        />
+      </View>
     );
-};
+  }
 
 export default Home;
+
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: '#240530',
+      alignItems: 'center',
+      justifyContent: 'center',
+    },
+    mainText: {
+      paddingTop: 50,
+      flex: 1,
+      color: '#ffffff',
+      alignItems: 'center',
+      justifyContent: 'center',
+    },
+    homeScreeBackground: { 
+      flex: 1, 
+      alignItems: 'center', 
+      justifyContent: 'center',
+      backgroundColor: '#240530', 
+    },
+    homeTitleText:{ 
+      color: '#fff',
+    },
+    ButtonText:{
+      textColor: '#1BD760',
+    }
+    
+  });
