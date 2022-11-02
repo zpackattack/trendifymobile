2f9d38f887c7ab8c3aeaa08a5ee89d005539925f
treyz3@icloud.com <Zpackattack>
2022-11-02 19:10:45 -0400

    Still working on api linkage

--- screens/Login.js
+++ screens/Login.js
@@ -3,12 +3,14 @@ import * as React from 'react';
 import {   StyleSheet,
     Text,
     View,
+    ActivityIndicator,
     Image,
     TextInput,
     Button,
     TouchableOpacity, } from 'react-native';
 import LogoHeader from '../components/LogoHeader';
 import axios from 'axios'; 
+import { useState } from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
@@ -16,46 +18,77 @@ import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
 function Login({navigation})
 {
+  const [message, setMessage] = useState();
+  const [messageType, setMessageType] = useState();
+
   const handleLogin = (credentials) => 
   {
-    const url = '';
+    const url = 'https://trendify-project.herokuapp.com/api/login';
+
+    axios
+      .post(url, credentials)
+      .then((response) => 
+      {
+        const result = response.data;
+        const {message, status, data} = result;
+
+        if (status !== 'SUCESS')
+        {
+          handleMessage(message, status);
+        }
+        else
+        {
+          navigation.navigate('app', {...data[0]});
+        }
+      })
+      .catch(error => {
+      console.log(error.JSON());
+      handleMessage("An error occurred. Check your network");
+    })
   }
-    return (
-        <View style={styles.container}>
-        <LogoHeader />
-    
 
-        <View style={styles.inputView}>
-            <TextInput
-            style={styles.TextInput}
-            placeholder="Email"
-            placeholderTextColor="#003f5c"
-            
-            />
-        </View>
-    
-        <View style={styles.inputView}>
-            <TextInput
-            style={styles.TextInput}
-            placeholder="Password"
-            placeholderTextColor="#003f5c"
-            secureTextEntry={true}
-            
-            />
-        </View>
-    
-        
-    
-        <TouchableOpacity style={styles.loginBtn}>
-            <Text style={styles.loginText} onPress={() => navigation.navigate('MainApp')}>Login</Text>
-        </TouchableOpacity>
+  const handleMessage = (message, type = 'FAILED') =>
+  {
+    setMessage(message);
+    setMessageType(type);
+  }
 
-        <TouchableOpacity>
-            <Text style={styles.registerTxt} onPress={() => navigation.navigate('Register')}>Don't have an account? Sign Up</Text>
-            
-        </TouchableOpacity>
-        </View>
-    );
+  return (
+      <View style={styles.container}>
+      <LogoHeader />
+  
+
+      <View style={styles.inputView}>
+          <TextInput
+          style={styles.TextInput}
+          placeholder="Email"
+          placeholderTextColor="#003f5c"
+          
+          />
+      </View>
+  
+      <View style={styles.inputView}>
+          <TextInput
+          style={styles.TextInput}
+          placeholder="Password"
+          placeholderTextColor="#003f5c"
+          secureTextEntry={true}
+          
+          />
+      </View>
+
+      <Text style={styles.messageBox} type={messageType}>{message}</Text>
+
+      <TouchableOpacity style={styles.loginBtn}>
+          <Text style={styles.loginText} onPress={handleLogin}>Login</Text>
+      </TouchableOpacity>
+
+      <TouchableOpacity>
+          <Text style={styles.registerTxt} onPress={() => navigation.navigate('Register')}>Don't have an account? Sign Up</Text>
+          
+      </TouchableOpacity>
+      </View>
+  );
 }
 
 export default Login;
@@ -101,5 +134,11 @@ const styles = StyleSheet.create({
       justifyContent: "center",
       marginTop: 20,
       backgroundColor: "#af40e3",
+    },
+    messageBox:
+    {
+      textAlign: 'center',
+      fontSize: 13,
+      color: 'red',
     },
   });
