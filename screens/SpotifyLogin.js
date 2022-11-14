1ae43d23df49932ec5248336f040d3a62f7587e0
treyz3@icloud.com <Zpackattack>
2022-11-14 17:15:01 -0500

    New Spotify login

--- screens/SpotifyLogin.js
+++ screens/SpotifyLogin.js
@@ -1,4 +1,4 @@
-import "react-native-gesture-handler";
+/*import "react-native-gesture-handler";
 import { useEffect } from "react";
 import {
     Button,
@@ -48,3 +48,52 @@ function SpotifyLogin({ navigation, route }) {
 }
 
 export default SpotifyLogin;
+*/
+
+import * as React from 'react';
+import * as WebBrowser from 'expo-web-browser';
+import { makeRedirectUri, ResponseType, useAuthRequest } from 'expo-auth-session';
+import { Button } from 'react-native';
+
+
+WebBrowser.maybeCompleteAuthSession();
+
+// Endpoint
+const discovery = {
+  authorizationEndpoint: 'https://accounts.spotify.com/authorize',
+  tokenEndpoint: 'https://accounts.spotify.com/api/token',
+};
+
+export default function SpotifyLogin({navigation}) {
+  const [request, response, promptAsync] = useAuthRequest(
+    {
+      responseType: ResponseType.Token,
+      clientId: 'f2654301008b4cba927766c7b3efdbcb',
+      scopes: ['Treyz3@icloud.com', 'playlist-modify-public'],
+      // In order to follow the "Authorization Code Flow" to fetch token after authorizationEndpoint
+      // this must be set to false
+      usePKCE: false,
+      redirectUri: makeRedirectUri({
+        scheme: 'exp://localhost:19000/--/'
+      }),
+    },
+    discovery
+  );
+
+  React.useEffect(() => {
+    if (response?.type === 'success') {
+      const { access_token } = response.params;
+      navigation.navigate('MainApp');
+    }
+  }, [response]);
+
+  return (
+    <Button
+      disabled={!request}
+      title="Login"
+      onPress={() => {
+        promptAsync();
+      }}
+    />
+  );
+}
